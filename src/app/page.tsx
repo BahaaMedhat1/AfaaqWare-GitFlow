8a100e0c3c94ab9e72e1c99dc64e8949d763401a
mazin189 <mazen.ahmed.abdelattii@gmail.com>
2026-03-28 21:26:44 +0200

    adding card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import Eslam from "@/components/organisms/Eslam";
 import ServiceSection from "@/components/organisms/ServiceSection";
 import TeamSection from "../components/organisms/TeamSection";
 import StatisticsSection from "@/components/organisms/StatisticsSection";
+import CardSection from "@/components/organisms/cardSection";
 
 export default function page() {
   return (
@@ -20,6 +21,7 @@ export default function page() {
       <ServiceSection />
       <TeamSection />
       <StatisticsSection />
+      <CardSection />
     </div>
   );
 }
