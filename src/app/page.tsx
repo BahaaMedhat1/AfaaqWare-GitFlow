50a48533ad0f924a277feb0353af149bc17256cd
Mohamed Mostafa <mohamed188882838@gmail.com>
2026-03-28 21:44:10 +0200

    add FAQSection components in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/organisms/HeroSection";
 import Navbar from "@/components/organisms/Navbar";
+import FAQSection from "@/components/organisms/faq-section";
 export default function page() {
   return (
     <div>
       <HeroSection />
       <Navbar />
+      <FAQSection/>
     </div>
   );
 }
