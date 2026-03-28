91058f879b0ee4a77396b94572780c9ab19c43bb
Reham-Abdelaleem <rehamabdelalem08@gmail.com>
2026-03-28 21:21:22 +0200

    add portfolio section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import HeroSection from "@/components/organisms/HeroSection";
 import Navbar from "@/components/organisms/Navbar";
+import PortfolioSection from "@/components/organisms/PortfolioSection";
+
 export default function page() {
   return (
     <div>
       <HeroSection />
       <Navbar />
+      <PortfolioSection />
     </div>
   );
 }
