3f6b67f5cf32af19c1a043923dee625b42f18a0c
eslam-mohamedl <ee.eslam355@gmail.com>
2026-03-28 20:29:43 +0200

    update project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,6 @@
-import Title from "@/components/atoms/Title";
-import Footer from "@/components/organisms/Footer";
-import Navbar from "@/components/organisms/Navbar";
+
 export default function page() {
   return (
-    <div>
-      <Title variant="primary" size="md">
-        this is title
-      </Title>
-      <Navbar />
-      <Footer />
-    </div>
-  );
+    <div>page</div>
+  )
 }
