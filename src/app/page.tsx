6b32e9f2352a7472a4132e57eccb4ef04520ca67
Yasmin Hillis <.com>
2026-03-28 21:18:23 +0200

    add statistics section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import HeroSection from "@/components/organisms/HeroSection";
 import Navbar from "@/components/organisms/Navbar";
 import TeamSection from "../components/organisms/TeamSection";
+import StatisticsSection from "@/components/organisms/StatisticsSection";
+
 export default function page() {
   return (
     <div>
       <HeroSection />
       <Navbar />
       <TeamSection />
+      <StatisticsSection />
     </div>
   );
 }
