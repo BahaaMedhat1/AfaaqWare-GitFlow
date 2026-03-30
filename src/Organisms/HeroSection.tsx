6409d73e84a0e99f24f293b83c0ec39307905ddf
Amal <amal.nasr.1421@gmail.com>
2026-03-30 15:53:24 +0200

    Remove Old Components

--- src/Organisms/HeroSection.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function HeroSection() {
-  return (
-    <div className='text-white font-bold text-2xl text-center bg-blue-700 mb-6 py-10'>HeroSection</div>
-  )
-}
