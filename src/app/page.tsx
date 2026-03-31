129989e3aaf95766d1b6ea320e32f479b0e86951
BahaaMedhat1 <bahaawanas427@gmail.com>
2026-04-01 00:09:22 +0200

    feat(ui): Add ModalComponent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Cart from "@/Organisms/Cart";
+import Modal from "@/Organisms/Modal";
 
 export default function page() {
   return (
     <div>
       <Cart />
+      <Modal />
     </div>
   );
 }
