580b0651310213a0335e2cda6d091f519dcb66bd
BahaaMedhat1 <bahaawanas427@gmail.com>
2026-04-01 00:20:45 +0200

    fix(search): Add path file for imported component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Cart from "@/Organisms/Cart";
 import Modal from "@/Organisms/Modal";
+import Search from "@/Organisms/Search";
 
 export default function page() {
   return (
     <div>
+      <Search />
       <Cart />
       <Modal />
     </div>
